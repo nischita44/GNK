6f27522c64eb3d92092c7e16fcca0f8502f0a9ff
nischita44 <nischitasadananda@gmail.com>
2024-09-17 06:39:46 -0400

    inoitial commit

---
+++ demo 3/src/main/java/com/example/demo/GnkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnkApplication.class, args);
+	}
+
+}
